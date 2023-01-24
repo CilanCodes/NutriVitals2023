f0b0d8ca1810896c6d977154d71c5a21eaa8066a
SealandCodes <sealandcodes@gmail.com>
2023-01-24 20:43:30 +0800

    add function `Home` `Buttons`

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] private Animator animator;
+
+    public Animator GetAnimator
+    {
+
+        get { return animator; }
+
+    }
+
+
     private void Start()
     {
         if(SceneManager.GetActiveScene().buildIndex == 1)
@@ -10,11 +21,12 @@ public class GameManager : MonoBehaviour
             Invoke("OpenHomeScreen", 5f);
         }
 
+        PlayerPrefs.SetInt("index", 2);
+
     }
 
     private void OpenHomeScreen()
     {
-        PlayerPrefs.SetInt("index", 2);
         SceneManager.LoadScene(2);
     }
 
@@ -24,10 +36,52 @@ public class GameManager : MonoBehaviour
         //SimpleInput.GetButton <-- Holding a Button
         //SimpleInput.GetButtonDown <-- Upon Pressing, execute
         //SimpleInput.GetButtonUp <-- Upon Release, execute
-        if (SimpleInput.GetButtonDown("OnRunNow"))
+        if (SimpleInput.GetButtonDown("OnRunNowGameScreen"))
         {
             PlayerPrefs.SetInt("index", 4);
             SceneManager.LoadScene(0);
         }
+
+        if (SimpleInput.GetButtonDown("OnFoodInformationScreen"))
+        {
+            //no need to call PlayerPrefs because your 
+            SceneManager.LoadScene(3);
+        }
+
+        if (SimpleInput.GetButtonDown("OnReturnHomeScreen"))
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptSettings"))
+        {
+            animator.SetTrigger("ActiveSettings");
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptAbout"))
+        {
+            animator.SetTrigger("ActiveAbout");
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptLeaderboard"))
+        {
+            animator.SetTrigger("ActiveLeaderboard");
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptDisable"))
+        {
+            animator.SetTrigger("InActivate");
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptGameOver"))
+        {
+    
+        }
+
+        if (SimpleInput.GetButtonDown("OnPromptPaused"))
+        {
+
+        }
+
     }
 }
