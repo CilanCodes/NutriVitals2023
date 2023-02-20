be7ad0df5d830fd2b8fe9af3fa33fab3a4692471
SealandCodes <sealandcodes@gmail.com>
2023-02-21 02:00:17 +0800

    enhance `Health and Energy HUD`

--- Assets/Scripts/Scr-GamePlay/HUDManager.cs
+++ Assets/Scripts/Scr-GamePlay/HUDManager.cs
@@ -9,15 +9,21 @@ public class HUDManager : MonoBehaviour
 {
     public static int scorePoints;
     public static float energyPoints;
+    public static float healthPoints;
     public static int goPoints;
     public static int growPoints;
     public static int glowPoints;
 
     [SerializeField] private Image energyBarFill;
+    [SerializeField] private Image healthBarFill;
     [SerializeField] private TextMeshProUGUI scoreTextPoints;
     [SerializeField] private TextMeshProUGUI goTextPoints;
     [SerializeField] private TextMeshProUGUI growTextPoints;
     [SerializeField] private TextMeshProUGUI glowTextPoints;
+
+    private float timeSinceLastDecrease = 0f;
+    [SerializeField] private float decreaseSpeed = .5f;
+    [SerializeField] private const float decreaseInterval = 2f;
 
 
     private void Start()
@@ -34,18 +40,26 @@ public class HUDManager : MonoBehaviour
 
         #region UPDATE HUD ELEMENTS
 
-        energyBarFill.fillAmount = energyPoints;
+        energyBarFill.fillAmount = Mathf.Lerp(energyBarFill.fillAmount, energyPoints, Time.deltaTime * 3f);
+
         scoreTextPoints.text = scorePoints.ToString();
+
         goTextPoints.text = goPoints.ToString();
         growTextPoints.text = growPoints.ToString();
         glowTextPoints.text = glowPoints.ToString();
 
+        if ((energyBarFill.fillAmount >= 0 && energyBarFill.fillAmount <= .17) || (energyBarFill.fillAmount >= .83 && energyBarFill.fillAmount <= 1))
+        {
+            DecreaseHealthBar();
+        }
+        
         #endregion
     }
 
     #region UPDATE FUNCTIONS
     public static void UpdateFoodPoints(int go, int grow, int glow)
     {
+
         goPoints += go;
         growPoints += grow;
         glowPoints += glow;
@@ -53,8 +67,34 @@ public class HUDManager : MonoBehaviour
 
     public static void UpdateScoreEnergyPoints(int score, float energy)
     {
-        scorePoints += score;
-        energyPoints += energy;
+        
+        //Determines if the score or energy reaches negative, if so reset the value to 0
+        if (scorePoints >= 0)
+        {
+            if((scorePoints+score) < 0) {
+                score = 0;
+                scorePoints = 0;
+            }
+            else
+            {
+                scorePoints += score;
+            }
+
+        }
+        if (energyPoints >= 0)
+        {
+            if ((energyPoints + energy) < 0)
+            {
+                energy = 0;
+                energyPoints = 0;
+            }
+            else
+            {
+                energyPoints += energy;
+            }
+            
+        }
+        
     }
     #endregion
 
@@ -80,4 +120,33 @@ public class HUDManager : MonoBehaviour
     #endregion
 
 
+
+    //SMOOTH HEALTH BAR
+    private void DecreaseHealthBar()
+    {
+        timeSinceLastDecrease += Time.deltaTime;
+
+        if (timeSinceLastDecrease >= decreaseInterval && healthBarFill.fillAmount > 0)
+        {
+            float targetFillAmount = healthBarFill.fillAmount - 0.1667f;
+            StartCoroutine(DecreaseHealthBarSmoothly(targetFillAmount, decreaseSpeed));
+            timeSinceLastDecrease = 0f;
+        }
+    }
+
+    private IEnumerator DecreaseHealthBarSmoothly(float targetFillAmount, float speed)
+    {
+        float startFillAmount = healthBarFill.fillAmount;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < speed)
+        {
+            timeElapsed += Time.deltaTime;
+            float lerpValue = timeElapsed / speed;
+            healthBarFill.fillAmount = Mathf.Lerp(startFillAmount, targetFillAmount, lerpValue);
+            yield return null;
+        }
+
+        healthBarFill.fillAmount = targetFillAmount;
+    }
 }
